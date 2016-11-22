0dc8df3353219b2c4f2498370a6010d672b93f8b
kounelios13 <pkounelios@gmail.com>
2016-11-22 22:30:11 +0200

    new abstract class

---
+++ src/com/AppUtils/gui/View.java
@@ -0,0 +1,23 @@
+package com.AppUtils.gui;
+
+import com.AppUtils.interfaces.IView;
+
+import interfaces.IView;
+import javax.swing.JFrame;
+@SuppressWarnings("serial")
+public abstract class View extends JFrame implements IView{
+	public View(String title,int width,int height){
+		this.setTitle(title);
+		this.setSize(width, height);
+	}
+	public View(String title,int width,int height,boolean resizable){
+		this(title,width,height);
+		this.setResizable(resizable);
+	}
+	public abstract String toString();
+	protected abstract void initUIElements();
+	@Override
+	public void toggleUI(){
+		this.setVisible(!this.isVisible());
+	}
+}	
