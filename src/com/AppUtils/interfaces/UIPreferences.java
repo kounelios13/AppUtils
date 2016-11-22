3ebfb96df281d55b181b132dc1cce56d58d2f176
kounelios13 <pkounelios@gmail.com>
2016-11-22 22:33:21 +0200

    new interface

---
+++ src/com/AppUtils/interfaces/UIPreferences.java
@@ -0,0 +1,19 @@
+package com.AppUtils.interfaces;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+public interface UIPreferences{
+	/**
+	 * An interface that defines basic rules that apply when you create a subprogram that changes UI settings such as colors(background,foreground)
+	 * or fonts
+	 */
+    Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+	/**
+	* Provide a way to the user for loading/saving preferences
+	**/
+    void loadPreferences();
+	void savePreferences();
+	void setBg(Color bg);
+	void setFg(Color fg);
+	void updatePreview();
+}
