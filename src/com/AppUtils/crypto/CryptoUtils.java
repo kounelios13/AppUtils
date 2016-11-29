780b2649e67d216d475bc20969d5f2a687625878
kounelios13 <pkounelios@gmail.com>
2016-11-29 10:57:18 +0200

    Added javadoc for crypto utils

--- src/com/AppUtils/crypto/CryptoUtils.java
+++ src/com/AppUtils/crypto/CryptoUtils.java
@@ -8,17 +8,49 @@ import javax.xml.bind.DatatypeConverter;
 
 public class CryptoUtils {
 
+	/**
+	 * 
+	 * Generate a 256 Hash
+	 * @param text Text to create the 256 hash from
+	 * @return hash value of given text
+	 * @throws NoSuchAlgorithmException Incorrect Hash Algorithm name
+	 * @throws UnsupportedEncodingException Encoding is not supported
+	 * */
 	public static String stringTo256Hash(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException{
 		return stringToSpecificHash("SHA-256",text);
 	}
+	/**
+	 * 
+	 * Generate a 512 Hash
+	 * @param text Text to create the 512 hash from
+	 * @return hash value of given text
+	 * @throws NoSuchAlgorithmException Incorrect Hash Algorithm name
+	 * @throws UnsupportedEncodingException Encoding is not supported
+	 * */
 	public static String stringTo512Hash(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException{
 		return stringToSpecificHash("SHA-512",text);
 	}
+	/**
+	 * 
+	 * Generate a 384 Hash
+	 * @param text Text to create the 384 hash from
+	 * @return hash value of given text
+	 * @throws NoSuchAlgorithmException Incorrect Hash Algorithm name
+	 * @throws UnsupportedEncodingException Encoding is not supported
+	 * */
 	public static String stringTo384Hash(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException{
 		return stringToSpecificHash("SHA-384",text);
 	}
-	public static String stringToSpecificHash(String hashAlgorithm,String text) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+	/**
+	 * Generate hash of text
+	 * @param hashAlgorithmName algorithm name
+	 * @param text text to hash
+	 * @return hash code of text
+	 * @throws NoSuchAlgorithmException Incorrect Hash Algorithm name
+	 * @throws UnsupportedEncodingException Encoding is not supported
+	 * */
+	private static String stringToSpecificHash(String hashAlgorithmName,String text) throws NoSuchAlgorithmException, UnsupportedEncodingException{
 		return DatatypeConverter.printHexBinary( 
-		           MessageDigest.getInstance(hashAlgorithm).digest(text.getBytes("UTF-8")));
+		           MessageDigest.getInstance(hashAlgorithmName).digest(text.getBytes("UTF-8")));
 	}
 }
