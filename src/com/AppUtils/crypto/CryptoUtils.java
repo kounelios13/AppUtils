db0975bdb5a7f2d5f0e7a2d7d960bb2226e66141
kounelios13 <pkounelios@gmail.com>
2016-12-04 21:25:14 +0200

    updated crypto utils and java doc

--- src/com/AppUtils/crypto/CryptoUtils.java
+++ src/com/AppUtils/crypto/CryptoUtils.java
@@ -49,8 +49,25 @@ public class CryptoUtils {
 	 * @throws NoSuchAlgorithmException Incorrect Hash Algorithm name
 	 * @throws UnsupportedEncodingException Encoding is not supported
 	 * */
+	
+	/**
+	 * Check if a given hash belongs to a given message
+	 * 
+	 * e.g. is the SHA-512 hash of foo F7FBBA6E0636F890E56FBBF3283E524C6FA3204AE298382D624741D0DC6638326E282C41BE5E4254D8820772C5518A2C5A8C0C7F7EDA19594A7EB539453E1ED7 ?
+	 * @param text The text to produce the hash from
+	 * @param hash the hash to compare to
+	 * @param hashAlgorithm the name of the hash algorithm to be used
+	 * @return true if the generated hash of text matches the given hash
+	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException
+	 */
+	public boolean isHashCorrect(String text,String hash ,String hashAlgorithm) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+		return stringToSpecificHash(hashAlgorithm,text).equals(hash);
+		
+	}
 	private static String stringToSpecificHash(String hashAlgorithmName,String text) throws NoSuchAlgorithmException, UnsupportedEncodingException{
 		return DatatypeConverter.printHexBinary( 
 		           MessageDigest.getInstance(hashAlgorithmName).digest(text.getBytes("UTF-8")));
 	}
+	
 }
